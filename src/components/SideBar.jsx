a4a721a55ff55a992e733327b50198761fdf52c6
Hrcode <117500660+Meagamer@users.noreply.github.com>
2024-09-27 10:23:45 +0530

    sidecontents/only

--- src/components/SideBar.jsx
+++ src/components/SideBar.jsx
@@ -1,26 +1,14 @@
 import PropTypes from "prop-types";
-import { GoHome } from "react-icons/go";
-import { GoHomeFill } from "react-icons/go";
-import { SiYoutubeshorts } from "react-icons/si";
-import { MdSubscriptions } from "react-icons/md";
-import { NavLink } from "react-router-dom";
 
-function SideBar({expand, isVisible}) {
+
+function SideBar({expand,}) {
   return (
-    <div className={`bg-background  h-screen ${expand ? 'max-w-[240px]' : 'max-w-[50px]'}
-        overflow-hidden p-5 relative`}>
-      <NavLink 
-      to="home" 
-      className={({ isActive }) => `
-        ${expand ? 'flex w-56 h-10 hover:bg-neautral items-center pl-3 left-2' : 'grid grid-rows-1 w-full h-12 p-1 left-0'}  ${isActive ? 'bg-neautral text-white' : 'bg-transparent'} 
-        rounded-lg absolute
-   
-      `}>
-          { expand? <GoHome className="w-7 h-7 mr-5"/> : <GoHomeFill className="w-7 h-7 mr-10 left"/> }
-          <p className={`text-text font-roboto pt-1  ${expand ? 'text-norm' : 'text-ss'}`}>Home</p>
-        </NavLink>
-    </div>
-  )
+    return (
+      <div className={`bg-background  h-screen ${expand ? 'max-w-[240px]' : 'max-w-[50px]'}
+          overflow-hidden pt-5 relative`}>
+         <SideContents expand={expand}/>
+      </div>
+    )
 };
 
 SideBar.propTypes = {
---
+++ src/components/SideContents.jsx
@@ -0,0 +1,31 @@
+import { GoHome } from "react-icons/go";
+import { GoHomeFill } from "react-icons/go";
+import { SiYoutubeshorts } from "react-icons/si";
+import { MdSubscriptions } from "react-icons/md";
+import { NavLink } from "react-router-dom";
+import PropTypes from "prop-types";
+
+
+
+
+
+function SideContents(expand) {
+
+  return (
+    <NavLink 
+    to="home" 
+    className={({ isActive }) => `
+      ${expand ? 'flex w-56 h-10 hover:bg-neautral items-center pl-3 left-2' : 'grid grid-rows-1 w-full h-12 '}  ${isActive ? 'bg-neautral text-white' : 'bg-transparent'} 
+      rounded-lg `}>
+        <GoHome className="w-7 h-7 mr-5 text-text"/>
+        <p className={`text-text font-roboto pt-1  ${expand ? 'text-norm' : 'text-ss'}`}>Home</p>
+      </NavLink>
+  )
+}
+
+SideContents.propTypes = {
+
+    expand: PropTypes.bool.isRequired, 
+  };
+
+export default SideContents;
